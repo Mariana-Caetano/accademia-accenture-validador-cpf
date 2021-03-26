c4c379caccd34c901dec7b6f7fefa602db694b72
Mariana Caetano Pereira <nanah.caetano@gmail.com>
2021-03-26 10:17:31 -0300

    alterations on the button step test

--- target/classes/static/script.js
+++ target/classes/static/script.js
@@ -1,9 +1,2 @@
 document.getElementById("buttonSend").addEventListener("click", validaFormulario)
 
-function validaFormulario(){
-  if (document.getElementById("fname").value != "" && document.getElementById("cpfId").value != "") {
-  alert("Obrigada!")
-  }else{
-    alert("Ops!Preencha os campos Nome e CPF.") 
-  }  
-}
